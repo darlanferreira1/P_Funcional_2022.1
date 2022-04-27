137bcb8785e5560f69ae1016819a8648b5c9781a
darlanferreira1 <darlanferreiradof7@gmail.com>
2022-04-26 22:43:21 -0300

    dream

--- aquecimento.hs
+++ aquecimento.hs
@@ -9,3 +9,27 @@ contEquals x y z =
         then 0
         else 2
 
+interior = drop 1 . reverse . drop 1 . reverse
+
+gangorra p1 c1 p2 c2 = 
+    if p1 * c1 == p2 * c2
+        then 0
+        else if p1 * c1 < p2 * c2
+        then 1
+        else -1
+
+
+min2 :: Ord p => p -> p -> p
+min2 x y =
+    if x < y
+        then x
+        else y
+
+soma2 x y = x + y
+
+
+min3 x y z =
+    min2 x (min2 y z)
+
+
+
