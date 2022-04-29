263fad9ecd8ba03a7c9487a745bc2e494d7fd139
Darlan Ferreira <darlanferreiradof7@gmail.com>
2022-04-29 04:53:51 -0300

    gg

---
+++ prova2.hs
@@ -0,0 +1,8 @@
+splitAtRec' [] pos = ([],0,[]) 
+splitAtRec' (x:xs) pos
+            | pos > 0 = (x:antes, elem, depois)
+            | pos == 0 = (antes, x , depois)
+            | otherwise =  (antes, elem, x:depois)
+            where (antes, elem, depois) = splitAtRec' xs (pos -1) 
+
+splitAtRec  xs pos = splitAtRec' xs pos 0 ([],0,[]) 
